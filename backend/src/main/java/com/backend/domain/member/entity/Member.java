43bc7502dc1eefbed35ed5413b06a8cab97ff875
yeongin <duddlsdlek610@gmail.com>
2025-10-14 11:21:51 +0900

    feat(member): Member 엔티티 설계 및 Role Enum 추가

--- backend/src/main/java/com/backend/domain/member/entity/Member.java
+++ backend/src/main/java/com/backend/domain/member/entity/Member.java
@@ -1,7 +1,41 @@
 package com.backend.domain.member.entity;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
+import lombok.*;
 
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
 @Entity
+@Table(name = "member")
 public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false, unique = true, length = 30)
+    private String memberId; // 로그인용 아이디
+
+    @Column(nullable = false)
+    private String password; // 암호화된 비밀번호
+
+    @Column(nullable = false, unique = true, length = 50)
+    private String email;
+
+    @Column(nullable = false, unique = true, length = 20)
+    private String nickname;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, length = 10)
+    private Role role;
+
+    public void updatePassword(String newPassword) {
+        this.password = newPassword;
+    }
+
+    public void updateNickname(String newNickname) {
+        this.nickname = newNickname;
+    }
 }
