65cc7d9bf9a07ae45437cc96c012521720ce8281
yeongin <duddlsdlek610@gmail.com>
2025-10-14 17:33:50 +0900

    feat(member): 회원가입 로직 구현

--- backend/src/main/java/com/backend/domain/member/repository/MemberRepository.java
+++ backend/src/main/java/com/backend/domain/member/repository/MemberRepository.java
@@ -4,7 +4,15 @@ import com.backend.domain.member.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface MemberRepository extends JpaRepository<Member, Long> {
+
+    Optional<Member> findByMemberId(String memberId);
+
+    boolean existsByMemberId(String memberId);
+    boolean existsByEmail(String email);
+    boolean existsByNickname(String nickname);
 
 }
