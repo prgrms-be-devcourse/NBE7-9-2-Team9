f1dd42ffeba3ae7d76d65ebb81aab8f66de07b60
junseokPP <pjs981130@gmail.com>
2025-10-14 17:16:08 +0900

    feat(place) : 카테고리 findAll() 리스트 기능 구현

--- backend/src/main/java/com/backend/domain/category/controller/CategoryController.java
+++ backend/src/main/java/com/backend/domain/category/controller/CategoryController.java
@@ -1,7 +1,26 @@
 package com.backend.domain.category.controller;
 
+import com.backend.domain.category.entity.Category;
+import com.backend.domain.category.service.CategoryService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
+@RequestMapping("/api/categories")
 public class CategoryController {
+
+    private  final CategoryService categoryService;
+
+    public CategoryController(CategoryService categoryService) {
+        this.categoryService = categoryService;
+    }
+
+    @GetMapping
+    public List<Category> findAll() {
+        return categoryService.findAll();
+    }
+
 }
--- backend/src/main/java/com/backend/domain/category/service/CategoryService.java
+++ backend/src/main/java/com/backend/domain/category/service/CategoryService.java
@@ -1,4 +1,21 @@
 package com.backend.domain.category.service;
 
+import com.backend.domain.category.entity.Category;
+import com.backend.domain.category.repository.CategoryRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class CategoryService {
+
+    private final CategoryRepository categoryRepository;
+
+    public CategoryService(CategoryRepository categoryRepository) {
+        this.categoryRepository = categoryRepository;
+    }
+
+    public List<Category> findAll() {
+        return categoryRepository.findAll();
+    }
 }
