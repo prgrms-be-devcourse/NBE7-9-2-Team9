d94053ca160f63bc6355458a9821ad3cbceef699
tobbot16 <tobbot16@naver.com>
2025-10-19 23:30:31 +0900

    feat(review): 리뷰 생성 기능  추가

--- backend/src/main/java/com/backend/domain/review/controller/ReviewController.java
+++ backend/src/main/java/com/backend/domain/review/controller/ReviewController.java
@@ -1,10 +1,31 @@
 package com.backend.domain.review.controller;
 
 
+import com.backend.domain.review.dto.ReviewRequestDto;
+import com.backend.domain.review.dto.ReviewResponseDto;
+import com.backend.domain.review.service.ReviewService;
+import com.backend.global.reponse.ApiResponse;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
+@RequiredArgsConstructor
 @RequestMapping("/api/review")
 public class ReviewController {
+
+    private final ReviewService reviewService;
+
+    //리뷰 등록
+    @PostMapping("/add")
+    public ApiResponse<ReviewResponseDto> createReview(@RequestBody ReviewRequestDto reviewRequestDto) {
+        ReviewResponseDto createdReview = reviewService.createReview(reviewRequestDto);
+        return ApiResponse.created(createdReview);
+    }
+
+
+
 }
