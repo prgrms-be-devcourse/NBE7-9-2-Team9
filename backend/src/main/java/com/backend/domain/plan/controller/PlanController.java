2c7bfc7019529d182a4ec2233720be76c17c6a58
Creamcheesepie <gnldbs1004@naver.com>
2025-10-19 11:23:56 +0900

    refact(plan):계획 컨트롤러 응답 ApiResponse<>로 일원화

--- backend/src/main/java/com/backend/domain/plan/controller/PlanController.java
+++ backend/src/main/java/com/backend/domain/plan/controller/PlanController.java
@@ -21,7 +21,7 @@ public class PlanController {
     private final PlanMemberService planMemberService;
 
     @PostMapping("/create")
-    public ResponseEntity<ApiResponse<PlanResponseBody>> create(
+    public ApiResponse<PlanResponseBody> create(
             @Valid @RequestBody PlanCreateRequestBody planCreateRequestBody
     ) {
 
@@ -30,22 +30,22 @@ public class PlanController {
 
         Plan plan = planService.createPlan(planCreateRequestBody, memberId);
         PlanResponseBody planResponseBody = new PlanResponseBody(plan);
-        return new ResponseEntity<>(ApiResponse.success(planResponseBody), HttpStatus.OK);
+        return ApiResponse.success(planResponseBody);
     }
 
     @GetMapping("/list")
-    public ResponseEntity<ApiResponse<List<PlanResponseBody>>> getList() {
+    public ApiResponse<List<PlanResponseBody>> getList() {
         //TODO 페이징 처리 적용하기, 일단은 전체 목록 조회
         //TODO JWT 토큰에서 멤버 아이디 정보 가져오기
         String memberId = "dummy";
 
         List<PlanResponseBody> plans = planService.getPlanList(memberId);
-        return new ResponseEntity<>(ApiResponse.success(plans), HttpStatus.OK);
+        return ApiResponse.success(plans);
 
     }
 
     @PatchMapping("/update/{planId}")
-    public ResponseEntity<ApiResponse<PlanResponseBody>> updatePlan(
+    public ApiResponse<PlanResponseBody> updatePlan(
             @Valid @RequestBody PlanUpdateRequestBody planUpdateRequestBody,
             @PathVariable long planId
     ) {
@@ -54,75 +54,60 @@ public class PlanController {
 
         PlanResponseBody planResponseBody = planService.updatePlan(planId, planUpdateRequestBody, memberId);
 
-        return new ResponseEntity<>(ApiResponse.success(planResponseBody), HttpStatus.OK);
+        return ApiResponse.success(planResponseBody);
     }
 
     @GetMapping("/{planId}")
-    public ResponseEntity<ApiResponse<PlanResponseBody>> getPlan(
+    public ApiResponse<PlanResponseBody> getPlan(
             @PathVariable long planId
     ) {
         PlanResponseBody planResponseBody = planService.getPlanResponseBodyById(planId);
-        return new ResponseEntity<>(ApiResponse.success(planResponseBody), HttpStatus.OK);
+        return ApiResponse.success(planResponseBody);
     }
 
     @DeleteMapping("/delete/{planId}")
-    public ResponseEntity<ApiResponse> deletePlan(
+    public ResponseEntity deletePlan(
             @PathVariable long planId
     ) {
         //TODO JWT 토큰에서 멤버 아이디 정보 가져오기
         String memberId = "dummy";
 
         planService.deletePlanById(planId, memberId);
-        return new ResponseEntity<>(ApiResponse.success(), HttpStatus.OK);
+        return new ResponseEntity<>( HttpStatus.OK);
     }
 
     @PostMapping("/member/invite")
-    public ResponseEntity<ApiResponse<PlanMemberResponseBody>> inviteMember(
+    public ApiResponse<PlanMemberResponseBody> inviteMember(
             @RequestBody PlanMemberAddRequestBody memberRequestBody
     ) {
         String memberId = "dummy";
         PlanMemberResponseBody planMemberResponseBody = planMemberService.invitePlanMember(memberRequestBody, memberId);
-        return new ResponseEntity<>(
-                ApiResponse.success(
-                        planMemberResponseBody),
-                HttpStatus.OK
-        );
+        return ApiResponse.success(planMemberResponseBody);
     }
 
     @GetMapping("/member/mylist")
-    public ResponseEntity<ApiResponse<List<PlanMemberMyResponseBody>>> getMyPlanMember(){
+    public ApiResponse<List<PlanMemberMyResponseBody>> getMyPlanMember(){
         String memberId = "dummy2";
-        return new ResponseEntity<>(
-                ApiResponse.success(
-                        planMemberService.myInvitedPlanList(memberId)
-                ),
-                HttpStatus.OK
-        );
+        return ApiResponse.success(planMemberService.myInvitedPlanList(memberId));
     }
 
     @PatchMapping("/member/accept")
-    public ResponseEntity<ApiResponse<PlanMemberResponseBody>> acceptMember(
+    public ApiResponse<PlanMemberResponseBody> acceptMember(
             @RequestBody PlanMemberAnswerRequestBody memberAnswerRequestBody
     ){
         String memberId = "dummy2";
         PlanMemberResponseBody planMemberResponseBody = planMemberService.acceptInvitePlanMember(memberAnswerRequestBody, memberId);
 
-        return new ResponseEntity<>(
-                ApiResponse.success(planMemberResponseBody),
-                HttpStatus.OK
-        );
+        return ApiResponse.success(planMemberResponseBody);
     }
 
     @PatchMapping("/member/deny")
-    public ResponseEntity<ApiResponse<PlanMemberResponseBody>> denyMember(
+    public ApiResponse<PlanMemberResponseBody> denyMember(
             @RequestBody PlanMemberAnswerRequestBody memberAnswerRequestBody
     ){
         String memberId = "dummy2";
         PlanMemberResponseBody planMemberResponseBody = planMemberService.denyInvitePlanMember(memberAnswerRequestBody, memberId);
 
-        return new ResponseEntity<>(
-                ApiResponse.success(planMemberResponseBody),
-                HttpStatus.OK
-        );
+        return ApiResponse.success(planMemberResponseBody);
     }
 }
