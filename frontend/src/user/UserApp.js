5802883fc7c1b5ec0b0f55829902518878bd8f32
junseokPP <pjs981130@gmail.com>
2025-10-20 14:26:32 +0900

    사용자 페이지 라우팅 구조 수정 및 스타일 개선
    - App.js 스타일 속성 수정 (따옴표 통일)
    - UserApp.js 파일 수정
    - 백엔드 .env 파일 변경사항 포함

--- frontend/src/user/UserApp.js
+++ frontend/src/user/UserApp.js
@@ -6,8 +6,8 @@ const UserApp = () => {
   return (
     <div className="user-app">
       <Routes>
-        <Route 
-          path="/" 
+        <Route
+          path="/"
           element={
             <div className="user-home">
               <h1>사용자 페이지</h1>
@@ -19,11 +19,17 @@ const UserApp = () => {
                 <a href="/user/member">회원</a>
               </div>
             </div>
-          } 
+          }
         />
-        <Route path="/plan/*" element={<div>여행 계획 페이지 (개발 예정)</div>} />
+        <Route
+          path="/plan/*"
+          element={<div>여행 계획 페이지 (개발 예정)</div>}
+        />
         <Route path="/review/*" element={<div>리뷰 페이지 (개발 예정)</div>} />
-        <Route path="/bookmark/*" element={<div>북마크 페이지 (개발 예정)</div>} />
+        <Route
+          path="/bookmark/*"
+          element={<div>북마크 페이지 (개발 예정)</div>}
+        />
         <Route path="/member/*" element={<div>회원 페이지 (개발 예정)</div>} />
       </Routes>
     </div>
