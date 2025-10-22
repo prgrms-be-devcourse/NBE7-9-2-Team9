7aae61da93b8dcd88631d4fc657609ea792aa871
yeongin <duddlsdlek610@gmail.com>
2025-10-22 10:08:53 +0900

    feat(member): 컨트롤러에 @AuthenticationPrincipal 도입

--- backend/src/main/java/com/backend/domain/member/controller/MemberController.java
+++ backend/src/main/java/com/backend/domain/member/controller/MemberController.java
@@ -6,7 +6,9 @@ import com.backend.domain.member.dto.request.MemberUpdateRequest;
 import com.backend.domain.member.dto.response.MemberResponse;
 import com.backend.domain.member.service.MemberService;
 import com.backend.global.reponse.ApiResponse;
+import com.backend.global.security.CustomUserDetails;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -16,36 +18,30 @@ public class MemberController {
 
     private final MemberService memberService;
 
-    /*@GetMapping("/me")
-    public ApiResponse<MemberResponse> getMyInfo(@AuthenticationPrincipal CustomUserDetails user) {
-        MemberResponse response = memberService.getMember(user.getMemberId());
-        return ApiResponse.success(response, "회원 조회 성공");
-    }*/
-
     @PostMapping("/signup")
     public ApiResponse<MemberResponse> signup(@RequestBody MemberSignupRequest request) {
         MemberResponse response = memberService.signup(request);
         return ApiResponse.success(response, "회원가입이 완료되었습니다");
     }
 
-    @GetMapping("/{id}")
-    public ApiResponse<MemberResponse> getMember(@PathVariable Long id) {
-        MemberResponse response = memberService.getMember(id);
-        return ApiResponse.success(response, "회원 조회 성공");
+    @GetMapping("/me")
+    public ApiResponse<MemberResponse> getMyInfo(@AuthenticationPrincipal CustomUserDetails member) {
+        MemberResponse response = memberService.getMember(member.getId());
+        return ApiResponse.success(response, "내 정보 조회 성공");
     }
 
-    @PatchMapping("/{id}")
+    @PatchMapping("/me")
     public ApiResponse<MemberResponse> updateMember(
-            @PathVariable Long id,
+            @AuthenticationPrincipal CustomUserDetails member,
             @RequestBody MemberUpdateRequest request
     ) {
-        MemberResponse response = memberService.updateMember(id, request);
+        MemberResponse response = memberService.updateMember(member.getId(), request);
         return ApiResponse.success(response, "회원정보가 수정되었습니다");
     }
 
-    @DeleteMapping("/{id}")
-    public ApiResponse<MemberResponse> deleteMember(@PathVariable Long id) {
-        MemberResponse response = memberService.deleteMember(id);
+    @DeleteMapping("/me")
+    public ApiResponse<MemberResponse> deleteMember(@AuthenticationPrincipal CustomUserDetails member) {
+        MemberResponse response = memberService.deleteMember(member.getId());
         return ApiResponse.success(response, "회원 탈퇴가 완료되었습니다.");
     }
 }
