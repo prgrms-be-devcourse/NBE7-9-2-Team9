fbd8b649f5050db0084a84a2d2f65b048ac615af
yeongin <duddlsdlek610@gmail.com>
2025-10-22 09:13:05 +0900

    feat(member): requestDto 일부 수정

--- backend/src/main/java/com/backend/domain/member/dto/request/MemberUpdateRequest.java
+++ backend/src/main/java/com/backend/domain/member/dto/request/MemberUpdateRequest.java
@@ -1,6 +1,13 @@
 package com.backend.domain.member.dto.request;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
 public record MemberUpdateRequest(
+
+        @Email(message = "이메일 형식이 올바르지 않습니다.")
         String email,
+
+        @NotBlank
         String nickname
 ) { }
