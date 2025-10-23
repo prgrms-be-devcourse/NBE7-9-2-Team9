378c32a1d9ec712c2797e1b22bead64129b7caca
Creamcheesepie <gnldbs1004@naver.com>
2025-10-23 10:20:20 +0900

    feat/plan-front: userApp 루트 관련 수정

--- frontend/src/user/plan/PlanApp.jsx
+++ frontend/src/user/plan/PlanApp.jsx
@@ -1,4 +1,4 @@
-// PlanApp.jsx
+// src/user/plan/PlanApp.jsx
 import { Routes, Route } from "react-router-dom";
 import PlanListPage from "./pages/PlanListPage";
 import PlanPage from "./pages/PlanPage";
@@ -7,10 +7,10 @@ import PlanCreatePage from "./pages/PlanCreatePage";
 export default function PlanApp() {
   return (
     <Routes>
-      <Route path="/" element={<PlanPage />} />
-      <Route path="create" element={<PlanCreatePage />}/>
+      {/* index = /user/plan */}
+      <Route index element={<PlanPage />} />
+      <Route path="create" element={<PlanCreatePage />} />
       <Route path="list" element={<PlanListPage />} />
-      <Route path="detail/:id" element={<div>여행 상세 페이지</div>} />
     </Routes>
   );
 }
