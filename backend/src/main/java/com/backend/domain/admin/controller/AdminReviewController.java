d667890eb31a4834d8e7e99686a78187a92845df
tobbot16 <tobbot16@naver.com>
2025-11-17 12:06:15 +0900

    feat: admin부분 review - 인증부분은 미구현

--- backend/src/main/java/com/backend/domain/admin/controller/AdminReviewController.java
+++ backend/src/main/java/com/backend/domain/admin/controller/AdminReviewController.java
@@ -1,11 +1,15 @@
-/*
 package com.backend.domain.admin.controller;
 
+
 import com.backend.domain.admin.service.AdminReviewService;
+import com.backend.domain.place.dto.ResponsePlaceDto;
 import com.backend.domain.review.dto.ReviewResponseDto;
-import com.backend.global.reponse.ApiResponse;
+import com.backend.global.response.ApiResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
@@ -16,44 +20,16 @@ public class AdminReviewController {
 
     private final AdminReviewService adminReviewService;
 
-    */
-/** 전체 리뷰 조회 *//*
-
-    @GetMapping
+    @GetMapping()
     public ApiResponse<List<ReviewResponseDto>> getAllReviews() {
         return ApiResponse.success(adminReviewService.getAllReviews());
     }
 
-    */
-/** 장소별 리뷰 조회 *//*
-
-    @GetMapping("/place/{placeId}")
-    public ApiResponse<List<ReviewResponseDto>> getReviewsByPlace(@PathVariable Long placeId) {
-        return ApiResponse.success(adminReviewService.getReviewsByPlace(placeId));
-    }
-
-    */
-/** 리뷰 단건 조회 *//*
-
-    @GetMapping("/{reviewId}")
-    public ApiResponse<ReviewResponseDto> getReview(@PathVariable Long reviewId) {
-        return ApiResponse.success(adminReviewService.getReview(reviewId));
-    }
-
-    */
-/** 리뷰 강제 삭제 *//*
 
     @DeleteMapping("/{reviewId}")
-    public ApiResponse<Void> deleteReview(@PathVariable Long reviewId) {
-        adminReviewService.forceDeleteReview(reviewId);
-        return ApiResponse.success(null, "리뷰 삭제 완료");
+    public ApiResponse<Void> deleteReview(Long reviewId) {
+        adminReviewService.deleteReview(reviewId);
+        return ApiResponse.success(null, "리뷰가 성공적으로 삭제되었습니다.");
     }
 
-    */
-/** 장소별 상위 리뷰 조회 *//*
-
-    @GetMapping("/top/{placeId}")
-    public ApiResponse<List<ReviewResponseDto>> getTopReviews(@PathVariable Long placeId) {
-        return ApiResponse.success(adminReviewService.getTopReviews(placeId));
-    }
-}*/
+}
--- backend/src/main/java/com/backend/domain/admin/service/AdminReviewService.java
+++ backend/src/main/java/com/backend/domain/admin/service/AdminReviewService.java
@@ -1,16 +1,9 @@
-/*
 package com.backend.domain.admin.service;
 
-import com.backend.domain.member.entity.Member;
-import com.backend.domain.member.repository.MemberRepository;
-import com.backend.domain.place.entity.Place;
+
 import com.backend.domain.place.repository.PlaceRepository;
 import com.backend.domain.review.dto.ReviewResponseDto;
-import com.backend.domain.review.entity.Review;
 import com.backend.domain.review.repository.ReviewRepository;
-import com.backend.domain.review.service.ReviewService;
-import com.backend.global.exception.BusinessException;
-import com.backend.global.reponse.ErrorCode;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -19,70 +12,28 @@ import java.util.List;
 
 @Service
 @RequiredArgsConstructor
-@Transactional
 public class AdminReviewService {
 
     private final ReviewRepository reviewRepository;
-    private final ReviewService reviewService;
-    private final MemberRepository memberRepository;
     private final PlaceRepository placeRepository;
 
-    */
-/** 전체 리뷰 조회 *//*
+    //전체 리뷰 조회
 
-    public List<ReviewResponseDto> getAllReviews() {
-        return reviewService.getAllReviews();
-    }
-
-    */
-/*
-    TODO: List<Review> findAllByMemberId(Long memberId); 필요
-    // 특정 회원의 리뷰 조회
-    public List<ReviewResponseDto> getReviewsByMember(Long memberId) {
-        Member member = memberRepository.findById(memberId)
-                .orElseThrow(() -> new BusinessException(ErrorCode.MEMBER_NOT_FOUND));
-
-        List<Review> reviews = reviewRepository.findByMemberId(member.getId());
-        return reviews.stream()
-                .map(review -> new ReviewResponseDto(review.getId(), review.getRating(), review.getModified_Date()))
-                .toList();
-    }
-    *//*
-
-
-    */
-/** 특정 장소의 리뷰 조회 *//*
-
-    public List<ReviewResponseDto> getReviewsByPlace(Long placeId) {
-        Place place = placeRepository.findById(placeId)
-                .orElseThrow(() -> new BusinessException(ErrorCode.NOT_FOUND_PLACE));
-
-        List<Review> reviews = reviewRepository.findByPlaceId(place.getId());
-        return reviews.stream()
-                .map(review -> new ReviewResponseDto(review.getId(), review.getRating(), review.getModified_Date()))
+    @Transactional
+    public List<ReviewResponseDto> getAllReviews(){
+        return reviewRepository.findAll()
+                .stream()
+                .map(ReviewResponseDto::from)
                 .toList();
     }
 
-    */
-/** 리뷰 단건 조회 *//*
-
-    public ReviewResponseDto getReview(Long reviewId) {
-        return reviewService.getReview(reviewId);
+    //리뷰 삭제
+    @Transactional
+    public void deleteReview(Long reviewId){
+        reviewRepository.deleteById(reviewId);
     }
 
-    */
-/** 리뷰 강제 삭제 *//*
 
-    public void forceDeleteReview(Long reviewId) {
-        Review review = reviewService.getReviewEntity(reviewId);
-        reviewRepository.delete(review);
-    }
 
-    */
-/** 장소별 상위 리뷰 조회 *//*
 
-    public List<ReviewResponseDto> getTopReviews(Long placeId) {
-        return reviewService.recommendByPlace(placeId);
-    }
 }
-*/
